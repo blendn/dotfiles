a35eec98aa2ae67a2cea839944b38168d260d9e6
Blendi Nuredini <blendn@protonmail.com>
2025-09-19 14:00:19 +0200

    fix(nvim): add alternative loading for nvim-treesitter with pcall

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,9 +16,16 @@ return {
         "sql",
         "svelte",
       },
+      highlight = { enable = true },
+      indent = { enable = true },
     },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      local ok, ts_configs = pcall(require, "nvim-treesitter")
+      if ok then
+        ts_configs.setup(opts)
+      else
+        vim.notify("Failed to load nvim-treesitter.configs", vim.log.levels.ERROR)
+      end
       -- MDX
       vim.filetype.add({
         extension = {
